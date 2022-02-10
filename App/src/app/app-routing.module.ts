057a8be737fc5ed0f4299e0768df07b1b886b04b
DESKTOP-M5UDMJM\Juan Carlos <juancarlosrodriguezlopez,jcr42@gmail.com>
2022-02-10 10:40:56 +0100

    Añadido App

---
+++ App/src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CalendarioComponent } from './components/vista/calendario/calendario.component';
+import { EventosComponent } from './components/vista/eventos/eventos.component';
+import { OfertasTrabajoComponent } from './components/vista/ofertas-trabajo/ofertas-trabajo.component';
+import { LoginComponent } from './components/vista/login/login.component';
+import { RegisterComponent } from './components/vista/register/register.component';
+import { GaleriaComponent } from './components/vista/galeria/galeria.component';
+import { LandingPageComponent } from './components/vista/landing-page/landing-page.component';
+import { AdminComponent } from './components/vista/admin/admin.component';
+
+
+
+const routes: Routes = [
+  { path : 'calendario', component:CalendarioComponent },
+  { path : 'eventos', component:EventosComponent },
+  { path : 'galeria', component:GaleriaComponent },
+  { path : 'trabajo', component:OfertasTrabajoComponent },
+  { path : 'login', component:LoginComponent },
+  { path : 'register', component:RegisterComponent },
+  { path : 'admin', component:AdminComponent },
+  { path : 'landing', component:LandingPageComponent },
+  { path: '', redirectTo: '/landing', pathMatch: 'full' }
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+
+})
+
+export class AppRoutingModule {}
---
+++ App/src/app/components/vista/ofertas-trabajo/ofertas-trabajo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ofertas-trabajo',
+  templateUrl: './ofertas-trabajo.component.html',
+  styleUrls: ['./ofertas-trabajo.component.css']
+})
+export class OfertasTrabajoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
